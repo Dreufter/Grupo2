0247c4c7bec9bf436e831f38e9c447ad40c5d2f2
javijm03 <47090862+javijm03@users.noreply.github.com>
2022-03-15 11:35:25 +0100

    Update EjercicioEntornosGit.java

--- EjercicioEntornosGit.java
+++ EjercicioEntornosGit.java
@@ -13,8 +13,6 @@ public class EjercicioEntornosGit {
         Libro libro1 = new Libro(123456, "La Ruta Prohibida",2007);
         Libro libro2 = new Libro(112233, "Los Otros", 2016);
         Libro libro3 = new Libro(456789, "La rosa del mundo", 1995);
-        Revista revista1 = new Revista(444555, "Año Cero", 2019, 344);
-        Revista revista2 = new Revista(002244, "National Geographic",2003,255);
         System.out.println(libro1);
         System.out.println(libro2);
         System.out.println(libro3);
